08ee9ce4f8307ae591fadd8c91bca85c76ac2d45
Bruna Girão <brunagirao.s@gmail.com>
2023-07-18 16:07:32 -0300

    populate picklist list in lwc component

--- force-app/main/default/lwc/createTicket/createTicket.js
+++ force-app/main/default/lwc/createTicket/createTicket.js
@@ -1,3 +1,42 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire }   from 'lwc';
 
-export default class CreateTicket extends LightningElement {}
+//UI OBJECTS INFO
+import { getPicklistValues }        from 'lightning/uiObjectInfoApi';
+import { getObjectInfo }            from 'lightning/uiObjectInfoApi';
+
+//OBJECTS
+import TICKET_OBJECT                from '@salesforce/schema/fr_Ticket__c'
+
+//FIELDS
+import PRIORITY_FIELD               from '@salesforce/schema/fr_Ticket__c.Priority__c'
+import STATUS_FIELD                 from '@salesforce/schema/fr_Ticket__c.Status__c'
+
+
+export default class CreateTicket extends LightningElement {
+
+    //WIRES SECTION
+    @wire(getObjectInfo, 
+        {
+            objectApiName: TICKET_OBJECT
+        }
+    )
+    ticketInfo;
+
+    @wire(getPicklistValues, 
+        {
+            recordTypeId: '$ticketInfo.data.defaultRecordTypeId',
+            fieldApiName: PRIORITY_FIELD
+        }
+    )
+    priorityPicklist;
+
+    @wire(getPicklistValues,
+        {
+            recordTypeId: '$ticketInfo.data.defaultRecordTypeId',
+            fieldApiName: STATUS_FIELD
+        }
+
+    )
+    statusPicklist;
+
+}
